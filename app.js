caacee4ef2b945f12a832b5b7a1baa52d6522f06
mygithubcoding <mysignupemail16@gmail.com>
2021-09-27 20:57:48 -0400

    refactored code

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+
 const app = express();
-const expressHbs = require("express-handlebars");
-
-// for handle-bars
-
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
-// app.set("view engine", "hbs");
-// app.set("views", "views");
-
-// The following for pug engine
-
-//app.set("view engine", "pug");
-//app.set("views", "views");
-
-// The following for ejs engine
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const path = require("path");
-const dotenv = require("dotenv");
-
-// no need to install body-parser and use app.use(bodyParser.urlencode())
-// with the below step, bodyparsing is and integral part of express
-//app.use(dotenv.config());
-//const PORT = process.env.PORT;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
-//another way of importing directly
-//app.use("/", require("./routes/admin"));
-// with the below line, every route starts with '/'
-// if we put app.use('/admin',adminRouter), the route starts with https://localhost:5000/admin
-
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use("/admin", adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+app.use(errorController.get404);
 
 app.listen(5000);
